e0ed57b7dcfc9b8306b18c65bc156a98db5d3386
Devi Krishna <devikrishnar2004@gmail.com>
2025-01-23 13:23:58 +0530

    first commit

---
+++ authapp/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from authapp.models import (
+    Contact,
+    MembershipPlan,
+    Enrollment,
+    Trainer,
+    Gallery,
+    Attendance, 
+    Biceptricep,
+    Legs,
+    Chests,
+    Body,
+    Shoulder, # Add this if `HeartRate` is part of authapp
+)
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(Gallery)
+admin.site.register(Attendance)
+admin.site.register(Biceptricep)
+admin.site.register(Legs)
+admin.site.register(Chests)
+admin.site.register(Body)
+admin.site.register(Shoulder)
+
+# Register HeartRate if it belongs to authapp
---
+++ authapp/urls.py
@@ -0,0 +1,47 @@
+from django.urls import include,path
+from . import views
+
+urlpatterns = [
+    path('',views.Home,name="Home"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handleLogout,name="handleLogout"),
+    path('contact',views.contact,name="contact"),
+    path('join',views.enroll,name="enroll"),
+    path('profile',views.profile,name="profile"),
+    path('gallery',views.gallery,name="gallery"),
+    path('attendance',views.attendance,name="attendance"),  
+    path('biceps/', views.biceps_pose, name='biceps_pose'),
+    path('benchpress/', views.benchpress, name='benchpress'),
+    path('chest/', views.chest, name='chest'),
+    path('lateralraise/', views.lateralraise, name='lateralraise'),
+    path('squats/', views.squats, name='squats'),
+    path('legraise/', views.legraise, name='legraise'),
+    path('shoulderpress/', views.shoulderpress, name='shoulderpress'),
+    path('api/', include('exercise_log.urls')),
+    path('russiantwist/', views.russiantwist, name='russiantwist'),
+    path('tricepdips/', views.tricepdips, name='tricepdips'),
+    path('triceppushdown/', views.triceppushdown, name='triceppushdown'),
+    path('romaniandeadlift/', views.romaniandeadlift, name='romaniandeadlift'),
+    path('lateralpulldown/', views.lateralpulldown, name='lateralpulldown'),
+    path('CHEST/' , views.ExerciseLog, name='exercises'),
+    path('exercisesbicep/', views.exercise_list, name='exercise_list'),
+    path('legs/', views.exercises_legs, name='exercises_legs'), 
+    path('chests/', views.exercises_chests, name='exercises_chests'),
+    path('body/', views.exercises_body, name='exercises_body'),
+    path('shoulder/', views.exercises_shoulder, name='exercises_shoulder'),
+    path('curlbicep/', views.tobicepcurlpage, name='tobicepcurlpage'),
+    path('shoulderpress1/', views.toshoulderpresspage, name='toshoulderpresspage'),
+    path('tricepdip1/', views.totricepdippage, name='totricepdippage'),
+    path('triceppushdown1/',views.totriceppushdownpage, name='totriceppushdownpage'),
+    path('squat1/' ,views.tosquatpage, name='tosquatpage'),
+    path('deadlift1/' ,views.todeadliftpage, name='todeadliftpage'),
+    path('russiantwist1/' ,views.torussiantwistpage, name='torussiantwistpage'),
+    path('legraise1/' ,views.tolegraisepage, name='tolegraisepage'),
+    path('lateralpulldown1/' ,views.tolateralpulldownpage, name='tolateralpulldownpage'),
+    path('pushup1/' ,views.topushuppage, name='topushuppage'),
+    path('lateralraise1/' ,views.tolateralraisepage, name='tolateralraisepage'),
+ # URL for biceps pose
+
+    
+]
